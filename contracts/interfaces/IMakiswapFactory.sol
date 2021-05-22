dc8998ddfd73f686891034662013fb8c909975e6
Uni <cryptounico@gmail.com>
2021-05-22 05:05:08 -0500

    update: _feeToSetter

--- contracts/interfaces/IMakiswapFactory.sol
+++ contracts/interfaces/IMakiswapFactory.sol
@@ -6,10 +6,10 @@ interface IMakiswapFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
     event SetFeeTo(address indexed user, address indexed _feeTo);
     event SetMigrator(address indexed user, address indexed _migrator);
-    event FeeToSetter(address indexed user, address indexed feeToSetter);
+    event FeeToSetter(address indexed user, address indexed _feeToSetter);
 
     function feeTo() external view returns (address _feeTo);
-    function feeToSetter() external view returns (address _fee);
+    function feeToSetter() external view returns (address _feeToSetter);
     function migrator() external view returns (address _migrator);
 
     function getPair(address tokenA, address tokenB) external view returns (address pair);
